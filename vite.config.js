f6969b04adcca6c566393cd9dc630fdc1b2e67e1
raykspro <rayanfelipe9882@gmail.com>
2026-04-12 20:58:34 -0300

    Correção de configuração no vite.config.js para evitar erro 404 no redirecionamento

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
   server: {
     host: "localhost",
     port: 3000,
