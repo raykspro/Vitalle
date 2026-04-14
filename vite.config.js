6a0c614dc8578f45a6e441f7c65f80a992e46471
raykspro <rayanfelipe9882@gmail.com>
2026-04-13 23:45:24 -0300

    Fix: Resolving App.jsx path and vite aliases for build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path' // Importante para os caminhos
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Vitalle Management System',
         short_name: 'Vitalle',
         description: 'Sistema de Gestão Vitalle',
-        theme_color: '#d946ef', // O seu Magenta
+        theme_color: '#d946ef',
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -26,5 +28,10 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Isso ensina o Vite o que significa o @
+    },
+  },
 })
