ad113b7ac2324fd427acb3171eb558ca74020de9
raykspro <rayanfelipe9882@gmail.com>
2026-04-13 22:01:48 -0300

    Correção: Remoção de imports inválidos para build

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
-import { clerk } from '@clerk/clerk-react';
-
 console.log('SISTEMA INICIADO');
-
-clerk.signOut();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
